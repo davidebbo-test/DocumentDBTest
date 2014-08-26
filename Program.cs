02008c9ad9f0c6ae15efd24169647cef50c3367f
davidebbo <david.ebbo@microsoft.com>
2014-08-26 11:41:21 -0700

    Simple test from ScottGu blog

--- Program.cs
+++ Program.cs
@@ -1,8 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
+using Newtonsoft.Json;
 
 namespace DocumentDBTest
 {
@@ -10,6 +16,49 @@ namespace DocumentDBTest
     {
         static void Main(string[] args)
         {
+            string endpoint = ConfigurationManager.AppSettings["endpoint"];
+            string authKey = ConfigurationManager.AppSettings["authKey"];
+
+            Test(endpoint, authKey).Wait();
+        }
+
+        static async Task Test(string endpoint, string authKey)
+        {
+            using (var client = new DocumentClient(new Uri(endpoint), authKey))
+            {
+                var database = new Database { Id = "ScottsDemoDB" };
+                database = await client.CreateDatabaseAsync(database);
+
+                var collection = new DocumentCollection { Id = "Families" };
+                collection = await client.CreateDocumentCollectionAsync(database.SelfLink, collection);
+
+                //DocumentDB supports strongly typed POCO objects and also dynamic objects
+                dynamic andersonFamily = JsonConvert.DeserializeObject(File.ReadAllText(@"..\..\Data\AndersonFamily.json"));
+                dynamic wakefieldFamily = JsonConvert.DeserializeObject(File.ReadAllText(@"..\..\Data\WakefieldFamily.json"));
+
+                //persist the documents in DocumentDB
+                await client.CreateDocumentAsync(collection.SelfLink, andersonFamily);
+                await client.CreateDocumentAsync(collection.SelfLink, wakefieldFamily);
+
+                //very simple query returning the full JSON document matching a simple WHERE clause
+                var query = client.CreateDocumentQuery(collection.SelfLink, "SELECT * FROM Families f WHERE f.id = 'AndersenFamily'");
+                var family = query.AsEnumerable().FirstOrDefault();
+
+                Console.WriteLine("The Anderson family have the following pets:");
+                foreach (var pet in family.pets)
+                {
+                    Console.WriteLine(pet.givenName);
+                }
+
+                //select JUST the child record out of the Family record where the child's gender is male
+                query = client.CreateDocumentQuery(collection.DocumentsLink, "SELECT * FROM c IN Families.children WHERE c.gender='male'");
+                var child = query.AsEnumerable().FirstOrDefault();
+
+                //Console.WriteLine("The Andersons have a son named {0} in grade {1} ", child.firstName, child.grade);
+
+                //cleanup test database
+                await client.DeleteDatabaseAsync(database.SelfLink);
+            }
         }
     }
 }
